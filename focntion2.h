7fee6eea56027e0c2f6b653303d7b50d720c31f5
Mehdi-Ben-Souda <elmehdisalahbensouda@hotmail.com>
2022-12-25 12:41:59 +0000

    implementation

--- focntion2.h
+++ focntion2.h
@@ -1,6 +1,27 @@
+#include "tpPileMathexp.h"
+union U_char_float
+{
+	float opperande;
+	char opperateur;
+};
+
+typedef struct Nd
+{
+	U_char_float cellule;
+	struct Nd * svt; 
+}Noeud;
+
+Noeud* Creer_element_pile(U_char_float opp)
 
 
 void Empiler_expression()
 {
+	Noeud * pile=NULL;
+	float entier;
+	char opperateur;
+	ele
+	entier=Caracters_To_float(&opperateur);
+	
+	
 	
 }
