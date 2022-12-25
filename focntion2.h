193e03edaf4214404471f201fad670fcdc625f1e
medjebb <92812220+medjebb@users.noreply.github.com>
2022-12-25 20:52:35 +0100

    changes

--- focntion2.h
+++ focntion2.h
@@ -33,9 +33,16 @@ Noeud *Empiler_element_pile(Noeud * pile,U_char_float element)
 	NE->svt=pile;
 	return (Noeud*)NE;	
 } 
+Noeud *Dempiler_element_pile(Noeud *pile)
+{
+	Noeud *ptr;
+	ptr=pile;
+	pile=pile->svt;
+	free(ptr);
+	return (Noeud*)pile;	
+} 
 
-
-void Empiler_expression()
+Noeud *Empiler_expression()
 {
 	Noeud * pile=NULL;
 	float reel;
@@ -43,18 +50,18 @@ void Empiler_expression()
 	U_char_float instance1;
 	do{
 		reel=Caracters_To_float(&opperateur);
-		printf("le reel : %f\n",reel);
-		printf("l'opperateur ' : %c\n",opperateur);
+//		printf("le reel : %f\n",reel);
+//		printf("l'opperateur ' : %c\n",opperateur);
 		instance1.opperande=reel;
-		Empiler_element_pile(pile,instance1);
+		pile=Empiler_element_pile(pile,instance1);
 		instance1.opperateur=opperateur;
 		if(opperateur!='\n')
-			Empiler_element_pile(pile,instance1);
+			pile=Empiler_element_pile(pile,instance1);
 		
 		
 	}while(opperateur!='\n');	
 	
-	
+	return((Noeud*)pile);
 }
  
 
@@ -71,26 +78,89 @@ float eval(U_char_float oprd1 , U_char_float opr , U_char_float oprd2)
 
 /* opr1 -> dernier operateur
    opr2 -> operateur lu */
-int priorite(char opr1,char op2)
+int priorite(char op1,char op2)
 {
 
-	if((opr1 =='*') || (opr1 == '/'))
+	if((op1 =='*') || (op1 == '/'))
 	{
-		if((opr2 =='*') || (opr2 == '/'))return((int)0)
-		return((int)1)
+		if((op2 =='*') || (op2 == '/'))return((int)0);
+		return((int)1);
 	}
 	return((int)0);
 
 }
 
 
-float calculer_exp_math_pile(Noeud *Pile_init)
+float calculer_exp_math_pile()
 {
-	if(Pile_init->svt==NULL)return(Pile_init->champ_opp);
+	Noeud *Pile_init,*Pile_opr,*Pile_eval;
+	U_char_float operande;
+	int val;
+	
+	//l'appel de la fonction Empiler_expression
+	printf("entrer une expression : ");
+	Pile_init=Empiler_expression();
+	
+	//le cas ou on a qu'un seul operande
+	if(Pile_init->svt==NULL)return(Pile_init->champ_opp.opperande);
+	
+	for(int i=0;i<3;i++)
+	{
+		if(i==1)
+		{
+			Pile_opr=Empiler_element_pile(Pile_opr,Pile_init->champ_opp);
+			Pile_init=Dempiler_element_pile(Pile_init);
+			continue;
+		}
+		Pile_eval=Empiler_element_pile(Pile_eval,Pile_init->champ_opp);
+		Pile_init=Dempiler_element_pile(Pile_init);
+	}
+	
 	while(Pile_init)
 	{
+		if(priorite(Pile_opr->champ_opp.opperateur,Pile_init->champ_opp.opperateur) == 0)
+		{
+			//l'empilement de l'operateur
+			Pile_opr=Empiler_element_pile(Pile_opr,Pile_init->champ_opp);
+			Pile_init=Dempiler_element_pile(Pile_init);
+			//l'empilement de l'operande
+			Pile_eval=Empiler_element_pile(Pile_eval,Pile_init->champ_opp);
+			Pile_init=Dempiler_element_pile(Pile_init);
+			continue;
+		}
+		operande=Pile_eval->champ_opp;
+		//depiler le 1er operande
+		Pile_eval=Dempiler_element_pile(Pile_eval);
+		//l'appel de la fonction eval
+		val=eval(operande,Pile_opr->champ_opp,Pile_eval->champ_opp);
+		//depiler le 2eme operande
+		Pile_eval=Dempiler_element_pile(Pile_eval);
+		//depiler le operateur
+		Pile_opr=Dempiler_element_pile(Pile_opr);
+		
+		operande.opperande=val;
+		//empiler le resultat dans la pile d'evaluation
+		Pile_eval=Empiler_element_pile(Pile_eval,operande);
+	}
+	
+	while(Pile_eval->svt)
+	{
+		operande=Pile_eval->champ_opp;
+		//depiler le 1er operande
+		Pile_eval=Dempiler_element_pile(Pile_eval);
+		//l'appel de la fonction eval
+		val=eval(operande,Pile_opr->champ_opp,Pile_eval->champ_opp);
+		//depiler le 2eme operande
+		Pile_eval=Dempiler_element_pile(Pile_eval);
+		//depiler le operateur
+		Pile_opr=Dempiler_element_pile(Pile_opr);
+		
+		operande.opperande=val;
+		//empiler le resultat dans la pile d'evaluation
+		Pile_eval=Empiler_element_pile(Pile_eval,operande);
 		
 	}
+	return((float)Pile_eval->champ_opp.opperande);
 }
 
 
--- fonctionPrincial.cpp
+++ fonctionPrincial.cpp
@@ -7,15 +7,10 @@
 int main(void)
 {
 	float val;
-	union U_char_float op1,op2,op3;
-	op1.opperande=5;
-	op2.opperateur='/';
-	op3.opperande=2;
-	 
 	
-	val=eval(op1,op2,op3);
-	printf("\nla valeur est : %f",val);
-	
+	val= calculer_exp_math_pile();
+	printf("\nle chiffre : %f",val);
+		
 	//float val=Caracters_To_float(&opr);
 	//printf("\nle chiffre : %+f",val);	
 	//printf("\nl\'oprateur  : %d",opr);
