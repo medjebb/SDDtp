8dcd50bc0497e7789efcdde692b0499b31f31ed6
medjebb <92812220+medjebb@users.noreply.github.com>
2022-12-25 12:22:26 +0100

    changes

--- tpPileMathexp.h
+++ tpPileMathexp.h
@@ -41,17 +41,9 @@ float Caracters_To_float(char *)
 	while( ( (cara=getchar()) != '.') || (cara != '\n')) 
 	{
 		unite=Est_numerique(cara);
-		//le cas ou le caractere
-		if(unite < 0)
-		{
-			if(!est_operateur(cara))
-			{
-				printf("ERREUR : expression mathematique est mal ecrite"); 
-				exit(0);
-			}
-			*opr=cara;
-			break;
-		}
+		//le cas ou le caractere n'est un caractere numerique
+		if(unite < 0)break;
+		
 		valeur1=valeur1*10+unite		
 	}	
 	
@@ -62,23 +54,26 @@ float Caracters_To_float(char *)
 		while((cara=getchar())!=(int)'\n')
 		{
 			unite=Est_numerique(cara);
-			if(unite < 0)
-			{
-				if(!est_operateur(cara))
-				{
-					printf("ERREUR : expression mathematique est mal ecrite"); 
-					exit(0);
-				}
-				*opr=cara;
-				break;
-			}
+			//le cas ou le caractere n'est un caractere numerique
+			if(unite < 0)break;
 			valeur2+=unite*rang;
 			rang/=10;
 		}
 	}
 	
-
+	
+	if(unite < 0)
+	{
+		if(!est_operateur(cara))
+		{
+			printf("ERREUR : expression mathematique est mal ecrite"); 
+			exit(0);
+		}
+		*opr=cara;
+	}
+	
 	if(cara == '\n')*opr=cara;
+	
 	// retourner la somme des deux parties du float
 	return (float)(signe*(valeur1+valeur2));
 }
