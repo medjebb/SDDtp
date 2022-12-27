5168667689530008fa9bd352b6621f10e7819737
medjebb <92812220+medjebb@users.noreply.github.com>
2022-12-27 23:29:59 +0100

    add the priorite function

---
+++ Implementation arbre/fonctionVer2.h
@@ -0,0 +1,13 @@
+
+/* op1 -> l'operateur qui se trouve dans le noeud courant
+   op2 -> operateur lu */
+int priorite(char op1,char op2)
+{
+	if((op1 =='+') || (op1 == '-'))
+	{
+		if((op2 =='-') || (op2 == '+'))return((int)0);
+		return((int)1);
+	}
+	return((int)0);
+
+}
