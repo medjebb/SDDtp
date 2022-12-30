743fa741e1f7cda835b76cd045af821b4dd9a393
Mehdi-Ben-Souda <elmehdisalahbensouda@hotmail.com>
2022-12-30 23:57:20 +0000

    X

--- Implementation pile/fonctionPrincial.cpp
+++ Implementation pile/fonctionPrincial.cpp
@@ -2,8 +2,6 @@
 #include "focntion2.h"
 
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(void)
 {
 	float val;
@@ -11,10 +9,7 @@ int main(void)
 	val= calculer_exp_math_pile();
 	printf("\nle chiffre : %f",val);
 		
-	//float val=Caracters_To_float(&opr);
-	//printf("\nle chiffre : %+f",val);	
-	//printf("\nl\'oprateur  : %d",opr);
-//	Empiler_expression();
+
 	return (int)0;	
 	
 }
